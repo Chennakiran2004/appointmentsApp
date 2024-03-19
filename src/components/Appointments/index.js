f8194c829d19c34507da509bc64db992895f45cf
kiran <kirankumarchenna2004@gmai.com>
2024-03-19 15:10:41 +0000

    Appointments App

---
+++ src/components/Appointments/index.js
@@ -0,0 +1,149 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+
+import {format} from 'date-fns'
+
+import './index.css'
+
+import AppointmentItem from '../AppointmentItem'
+
+class Appointments extends Component {
+  state = {
+    titleInput: '',
+    dateInput: '',
+    appointmentsList: [],
+    isFilterActive: false,
+  }
+
+  onChangeTitleInput = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  onChangeDateInput = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  toggleIsStarred = id => {
+    this.setState(prevState => ({
+      appointmentsList: prevState.appointmentsList.map(eachAppointment => {
+        if (id === eachAppointment.id) {
+          return {...eachAppointment, isStarred: !eachAppointment.isStarred}
+        }
+        return eachAppointment
+      }),
+    }))
+  }
+
+  onAddAppointment = event => {
+    event.preventDefault()
+    const {titleInput, dateInput} = this.state
+
+    const formattedDate = dateInput
+      ? format(new Date(dateInput), 'dd MMMM yyyy, EEEE')
+      : ''
+
+    const newAppointment = {id: v4(), title: titleInput, date: formattedDate}
+
+    this.setState(prevState => ({
+      appointmentsList: [...prevState.appointmentsList, newAppointment],
+      titleInput: '',
+      dateInput: '',
+    }))
+  }
+
+  onFilter = () => {
+    const {isFilterActive} = this.state
+
+    this.setState({
+      isFilterActive: !isFilterActive,
+    })
+  }
+
+  getFilteredAppointmentsList = () => {
+    const {appointmentsList, isFilterActive} = this.state
+
+    if (isFilterActive) {
+      return appointmentsList.filter(
+        eachAppointment => eachAppointment.isStarred === true,
+      )
+    }
+    return appointmentsList
+  }
+
+  render() {
+    const {titleInput, dateInput, appointmentsList, isFilterActive} = this.state
+
+    const isFilteredActiveClassName = isFilterActive
+      ? 'filter-filled'
+      : 'filter-empty'
+
+    const filteredAppointmentsList = this.getFilteredAppointmentsList(
+      appointmentsList,
+    )
+
+    return (
+      <div className="app-container">
+        <div className="responsive-container">
+          <div className="appointment-container">
+            <div className="add-appointment-container">
+              <form className="form" onSubmit={this.onAddAppointment}>
+                <h1 className="heading">Add Appointment</h1>
+                <label htmlFor="title" className="input-element-heading">
+                  TITLE
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  value={titleInput}
+                  onChange={this.onChangeTitleInput}
+                  className="input-element"
+                  placeholder="Title"
+                />
+                <label htmlFor="date" className="input-element-heading">
+                  DATE
+                </label>
+                <input
+                  type="date"
+                  id="date"
+                  value={dateInput}
+                  onChange={this.onChangeDateInput}
+                  className="input-element"
+                />
+                <button type="submit" className="add-button">
+                  Add
+                </button>
+              </form>
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+                alt="appointments"
+                className="appointment-image"
+              />
+            </div>
+            <hr className="line" />
+            <div className="header-with-filter-container">
+              <h1 className="appointments-heading">Appointments</h1>
+              <button
+                type="button"
+                className={`filter-style ${isFilteredActiveClassName}`}
+                onClick={this.onFilter}
+              >
+                Starred
+              </button>
+            </div>
+            <ul className="appointments-list">
+              {filteredAppointmentsList.map(eachAppointment => (
+                <AppointmentItem
+                  key={eachAppointment.id}
+                  appointmentDetails={eachAppointment}
+                  toggleIsStarred={this.toggleIsStarred}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
